609d9b2884af237cf085c7dcb2bef7f8bf2354c4
Rohit Pratti <rohit.pratti@gmail.com>
2019-05-18 02:06:29 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,6 +85,7 @@ def adding_matz
         }
      }
 
+   return programmer_hash
     
 end
 
@@ -108,7 +109,8 @@ def changing_alan
      alans_new_info = "GUI"
      
      programmer_hash[:alan_kay][:known_for] = alans_new_info
-     
+    
+    return programmer_hash
      
 end
 
